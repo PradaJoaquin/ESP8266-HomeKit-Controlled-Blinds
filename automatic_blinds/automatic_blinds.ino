2c9ff4260d5100881c5c53176ae0280607d0956f
Joaquin Prada <joaquiprada@gmail.com>
2023-07-30 20:56:45 -0300

    Physical & homekit switch Logic implemented

--- automatic_blinds/automatic_blinds.ino
+++ automatic_blinds/automatic_blinds.ino
@@ -8,10 +8,12 @@ void setup() {
 	Serial.begin(9600); // Modify to the bps of your device
 	wifi_connect();
 	my_homekit_setup();
+  pin_setup();
   // homekit_storage_reset(); // Uncomment this if you want to reset the device. As for pairing from another phone.
 }
 
 void loop() {
+  physical_switch_loop();
 	my_homekit_loop();
 	delay(10);
 }
@@ -19,34 +21,96 @@ extern "C" homekit_server_config_t config;
 extern "C" homekit_characteristic_t cha_switch_on;
 
 static uint32_t next_heap_millis = 0;
+static bool is_physical_switch_in_control = true;
 
+#define PIN_BLIND_MOTOR_DOWN D0       // Tells the blind motor to go down
+#define PIN_BLIND_MOTOR_UP D2         // Tells the blind motor to go up
+#define PIN_PHYSICAL_SWITCH_UP D6     // Turns ON when the physical switch is UP
+#define PIN_PHYSICAL_SWITCH_DOWN D8   // Turns ON when the physical switch is DOWN
 
-#define PIN_SWITCH D2
-
-void cha_switch_on_setter(const homekit_value_t value) {
-	bool on = value.bool_value;
-	cha_switch_on.value.bool_value = on;
-	LOG_D("Switch: %s", on ? "ON" : "OFF");
- 
-	digitalWrite(PIN_SWITCH, on ? HIGH : LOW);
+void pin_setup() {
+  pinMode(PIN_BLIND_MOTOR_DOWN, OUTPUT);
+  pinMode(PIN_BLIND_MOTOR_UP, OUTPUT);
+  pinMode(PIN_PHYSICAL_SWITCH_UP, INPUT_PULLUP);
+  pinMode(PIN_PHYSICAL_SWITCH_DOWN, INPUT_PULLUP);
+  
+	digitalWrite(PIN_BLIND_MOTOR_DOWN, LOW); // TODO: Revisar si el LOW enciende la conexión y hay que invertirlo cambiandolo con un HIGH.
+  digitalWrite(PIN_BLIND_MOTOR_UP, LOW); // TODO: Revisar si el LOW enciende la conexión y hay que invertirlo cambiandolo con un HIGH.
 }
 
 void my_homekit_setup() {
-	pinMode(PIN_SWITCH, OUTPUT);
-	digitalWrite(PIN_SWITCH, HIGH);
-
 	cha_switch_on.setter = cha_switch_on_setter;
 	arduino_homekit_setup(&config);
+}
 
+bool is_physical_switch_on_neutral() {
+  return digitalRead(PIN_PHYSICAL_SWITCH_UP) == LOW && digitalRead(PIN_PHYSICAL_SWITCH_DOWN) == LOW;
+}
+
+void cha_switch_on_setter(const homekit_value_t value) {
+  if (!is_physical_switch_on_neutral()) {
+    // We always prioritize the physical switch over the homekit switch.
+    LOG_D("Physical Switch is ON, change the position of the physical switch to neutral to use the homekit switch.");
+    return;
+  }
+  is_physical_switch_in_control = false;
+
+  bool on = value.bool_value;
+	cha_switch_on.value.bool_value = on;
+
+  LOG_D("Homekit Switch: %s", on ? "ON" : "OFF");
+
+	if (on) {
+    blind_motor_up();
+  } else {
+    blind_motor_down();
+  }
+}
+
+void blind_motor_up() {
+  digitalWrite(PIN_BLIND_MOTOR_DOWN, LOW);
+  delay(50);  // To make sure the other pin is LOW before chaging the other to HIGH.
+  digitalWrite(PIN_BLIND_MOTOR_UP, HIGH);
+}
+
+void blind_motor_down() {
+  digitalWrite(PIN_BLIND_MOTOR_UP, LOW);
+  delay(50);  // To make sure the other pin is LOW before chaging the other to HIGH.
+  digitalWrite(PIN_BLIND_MOTOR_DOWN, HIGH);
+}
+
+void blind_motor_off() {
+  digitalWrite(PIN_BLIND_MOTOR_UP, LOW);
+  digitalWrite(PIN_BLIND_MOTOR_DOWN, LOW);
+}
+
+void physical_switch_loop() {
+  if (digitalRead(PIN_PHYSICAL_SWITCH_UP) == HIGH && !is_physical_switch_in_control) {
+    LOG_D("Physical Switch: UP");
+    is_physical_switch_in_control = true;
+    blind_motor_up();
+  }
+  else if (digitalRead(PIN_PHYSICAL_SWITCH_DOWN) == HIGH && !is_physical_switch_in_control) {
+    LOG_D("Physical Switch: DOWN");
+    is_physical_switch_in_control = true;
+    blind_motor_down();
+  }
+  else {
+    if (is_physical_switch_in_control) {
+      LOG_D("Physical Switch: NEUTRAL");
+      is_physical_switch_in_control = false;
+      blind_motor_off();
+    }
+  }
 }
 
 void my_homekit_loop() {
 	arduino_homekit_loop();
+  
 	const uint32_t t = millis();
 	if (t > next_heap_millis) {
 		next_heap_millis = t + 5 * 1000;
 		LOG_D("Free heap: %d, HomeKit clients: %d",
 				ESP.getFreeHeap(), arduino_homekit_connected_clients_count());
-
 	}
 }
